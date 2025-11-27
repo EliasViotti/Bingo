179f055522221b2feb781269def5e12e85c0edb0
EliasViotti <elias_viotti_95@hotmail.com.ar>
2025-11-27 19:59:10 -0300

    Agregado los eventos para Juego Ganado y Numero Sorteado

---
+++ app/Events/NumeroSorteado.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NumeroSorteado implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public $codigoJuego;
+    public $numero;
+    public $numerosSorteados;
+    public function __construct($codigoJuego, $numero, $numerosSorteados)
+    {
+        $this->codigoJuego = $codigoJuego;
+        $this->numero = $numero;
+        $this->numerosSorteados = $numerosSorteados;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): Channel   
+    {
+        return new Channel('bingo.' . $this->codigoJuego);
+    }
+
+    public function broadcastAs()
+    {
+        return 'numero.sorteado';
+    }
+}
