611feeeecdc7df0b9bee212ec5495ffdf389f31b
EliasViotti <elias_viotti_95@hotmail.com.ar>
2025-11-27 20:46:56 -0300

    Agregadas las rutas (Juego nuevo, controlar el juego, sortear, crear tarjeta y verificar ganador)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JuegoController;
+use App\Http\Controllers\TarjetaController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('bingo')->name('bingo')->group(function () {
+    //creamos un juego nuevo
+    Route::post('/juego/crear', [JuegoController::class, 'crear'])->name('juego.crear');
+
+    //vista para controlar el juego
+    Route::get('/control/{codigo}', [JuegoController::class, 'control'])->name('control');
+
+    //sorteamos numero
+    Route::post('/juego/{codigo}/sortear', [JuegoController::class, 'sortearNumero'])->name('sortear');
+
+    //creamos tarjeta para jugar
+    Route::get('/tarjeta/{codigoJuego}', [TarjetaController::class, 'create'])->name('tarjeta');
+
+    //verificamos al ganador
+    Route::post(
+        '/juego/{codigoJuego}/tarjeta/{tarjetaId}/verificar',
+        [TarjetaController::class, 'verificarGanador']
+    )->name('verificar');
 });
